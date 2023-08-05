bf66d7b50364a9a8f29a15ac12b5bc338f0f0027
Min <als904204@naver.com>
2023-08-05 14:18:32 +0900

    init

---
+++ src/test/java/com/crud/querydsl/QueryDslApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.querydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryDslApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
